58a460daf45df4bbba04f31558f0ec1e8287239f
rohanravula <109545469+rohanravula@users.noreply.github.com>
2022-07-18 23:58:11 +0530

    Add files via upload

---
+++ comments_in.py
@@ -0,0 +1,9 @@
+"""
+Name:Rohan
+Date_&_Day:18th_july_2022_(Monday)
+File Description:Types_of_comments_in_python
+                  1)_Single_line_comment
+                  2)_Multi_line_comment
+"""
+
+print("Today we learnt the comments. And how many types of comments")
